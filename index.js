f8475971340eecc36f2db6dc0efbe9359ed8e85c
Pham Van Tam <pham.van.tam@azoom.jp>
2024-07-18 22:36:41 +0700

    :+1: Multer

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Thiết lập cấu hình lưu trữ cho Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // Thư mục lưu trữ tệp
+  },
+  filename: function (req, file, cb) {
+    // Đặt tên tệp: sử dụng tên gốc của tệp và thêm timestamp
+    cb(null, file.originalname)
+  }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// Khởi tạo Multer với cấu hình lưu trữ
+const upload = multer({ storage: storage })
+
+app.post('/file', upload.single('file'), (req, res) => {
+  res.send('File uploaded successfully!')
+})
+
+// Endpoint để xử lý tải lên nhiều tệp với cùng tên trường
+app.post('/photos', upload.array('photos', 12), (req, res) => {
+  res.send('Files uploaded successfully!')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
 })
